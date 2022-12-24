1ee61f89be6665bbfbcda796e3d47379da84d707
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2022-12-24 16:59:31 +0100

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let addIngredient = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach(ingredient => {
+  let items =  document.createElement("li");
+  items.className = 'item';
+  items.innerHTML=ingredient;
+  addIngredient.append(items);
+  addIngredient.appendChild(items);
+});
+
+
+
+
