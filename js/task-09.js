76e8cdff212b233b6bd2787fc528e2d5ecb8ce3b
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2022-12-26 10:12:54 +0100

    add task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const button = document.querySelector(".change-color");
+const newColor = document.querySelector(".color");
+button.addEventListener("click", () =>{
+  document.body.style.backgroundColor = getRandomHexColor();
+  newColor.textContent = document.body.style.backgroundColor;
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+
+
