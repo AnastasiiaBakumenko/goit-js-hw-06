8329e0873ff5fa4a4228a233c452faa9880067ce
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2022-12-26 09:36:54 +0100

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+const formInput = document.querySelector('.login-form input')
+
+form.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event){
+    event.preventDefault();
+const {
+    elements:{email, password}
+} = event.currentTarget;
+    if(email.value === "" || password.value === ""){
+        return alert("Введіть всі дані");
+    }
+    console.log(email.name, email.value);
+    console.log(password.name, password.value);
+    event.currentTarget.reset();
+}
+
 
